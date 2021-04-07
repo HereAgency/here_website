b459ca3cd3dba6224d30446b922d745fa27f2144
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-07 18:47:10 +1000

    changed loader

--- page_preloading_effect/js/main.js
+++ page_preloading_effect/js/main.js
@@ -12,8 +12,8 @@
 
 	var support = { animations : Modernizr.cssanimations },
 		container = document.getElementById( 'ip-container' ),
-		header = container.querySelector( 'header.ip-header' ),
-		loader = new PathLoader( document.getElementById( 'ip-loader-circle' ) ),
+		header = container.querySelector( '.ip-header' ),
+		loader = document.getElementById( 'ip-loader-circle' ),
 		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
 		// animation end event name
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
