c04d54ab03653a3a3e93887346bc27fd97196490
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-09 12:58:51 +1000

    Update img-text_animation.js
    inView.threshold to 0.2;

--- js_files/img-text_animation.js
+++ js_files/img-text_animation.js
@@ -1,137 +1,146 @@
-$('document').ready(function(){
-  
+$('document').ready(function () {
+  //Triggers the header animation after 3s (until the loading animation is finished)
   setTimeout(() => {
-      textHeader();
+    textHeader();
   }, 3000);
 
-  inView(".text-wrapper")
-    .on("enter", function (elText){
-    //Only triggers once for each Text Box
-    if (elText.classList.contains("hasClassTriggered")){
+  //Triggers images and texts (only once) when in viewport:
+  inView('.text-wrapper').on('enter', function (elText) {
+    if (elText.classList.contains('hasClassTriggered')) {
       return;
-    }else{
+    } else {
       textTrigger(elText);
-      elText.classList.add("hasClassTriggered"); 
+      elText.classList.add('hasClassTriggered');
     }
   });
-  inView.threshold(0.5);
-  inView(".image-wrapper").on("enter", function (elImg){
-    if (elImg.classList.contains("hasClassTriggered")){
+  inView.threshold(0.2);
+  inView('.image-wrapper').on('enter', function (elImg) {
+    if (elImg.classList.contains('hasClassTriggered')) {
       return;
-    }else{
+    } else {
       revealImage(elImg);
-      elImg.classList.add("hasClassTriggered"); 
+      elImg.classList.add('hasClassTriggered');
     }
   });
-	// "use strict";
-  
-/*  const scroller = new LocomotiveScroll({
-    el: document.querySelector('.scroll-container'),
-    smooth: true
-  })
-*/
 
-  //Global GSAP Timeline
-	// var tlTextHeader = gsap.timeline();
-  function textHeader(){
-    //local timeline
-    var tlTextHeader = gsap.timeline();
-    
-    var text = document.querySelectorAll(".text-animation-header");
-    var splitLine = document.querySelector(".split-child")
-    //SplitText to work with animation line by line
-    // const childSplit = nestedLinesSplit(text, {
-    //   type: "lines",
-    //   linesClass: "split-child"
-    // }); 
-    const childSplit = new SplitText(text, {
-      type: "lines",
-      linesClass: "split-child"
-    });
-    const parentSplit = new SplitText(text, {
-      linesClass: "split-parent"
-    });
-		// GSAP timeline to animate the text
-    //Make the text appear first
-    
-    tlTextHeader.to(text,{
-      duration:0.5,
-      opacity:1
-    })
-      .from(childSplit.lines,{
-      duration: 1,
-      yPercent: 100,
-      ease: "power4",
-      stagger: 0.15
-    }, ">");
-  }
-  
-  function textTrigger(element){
-    //local timeline
-    var tlText = gsap.timeline();
-    
-    var text = element.querySelector(".text-animation");
-    var splitLine = element.querySelector(".split-child")
-    //SplitText to work with animation line by line
-    // const childSplit = nestedLinesSplit(text, {
-    //   type: "lines",
-    //   linesClass: "split-child"
-    // }); 
-    const childSplit = new SplitText(text, {
-      type: "lines",
-      linesClass: "split-child"
-    });
-    const parentSplit = new SplitText(text, {
-      linesClass: "split-parent"
-    });
-		// GSAP timeline to animate the text
-    //Make the text appear first
-    
-    tlText
-      .to(text,{
-      duration:0.5,
-      opacity:1
-    })
-      .from(childSplit.lines,{
-      duration: 1,
-      yPercent: 100,
-      ease: "power4",
-      stagger: 0.15
-    }, ">")
-    ;
-	}
-
-	function revealImage(element) {
-		var reveal = element.querySelector('.revealer');
-		var img = element.querySelector('.img-animation');
-		// GSAP timeline to animate the reveal box and the image
-    // 		//First make the revealBox visible
-    
+  function textHeader() {
   //local timeline
-	var tl = gsap.timeline();
-		tl.to(reveal,{
-      duration:0.01,
-			opacity: 1
-		})
- 		//Animate the reveal box from the bottom to the full height
-		.from(reveal, {
-      duration:0.5,
-			transform: 'scale3d(1,0,1)',
-			transformOrigin: '50% 100%',
-			ease: Power4.easeOut,
-		}, ">")
-    // Make the image visibile
-		.to(img, {
-      duration:0.01,
-      opacity:1
-		}, ">")
-    //Animate the reveal box from full height to the top with no height
-		.to(reveal, {
-		  duration: 0.5,
-			transform: 'scale3d(1,0,1)',
-			transformOrigin: '50% 0%',
-			ease: Power4.easeOut
-		}, ">");
-	}
+  var tlTextHeader = gsap.timeline();
 
+  var text = document.querySelectorAll('.text-animation-header');
+  var splitLine = document.querySelector('.split-child');
+  //SplitText to work with animation line by line
+  // const childSplit = nestedLinesSplit(text, {
+  //   type: "lines",
+  //   linesClass: "split-child"
+  // });
+  const childSplit = new SplitText(text, {
+    type: 'lines',
+    linesClass: 'split-child',
+  });
+  const parentSplit = new SplitText(text, {
+    linesClass: 'split-parent',
+  });
+  // GSAP timeline to animate the text
+  //Make the text appear first
+
+  tlTextHeader
+    .to(text, {
+      duration: 0.5,
+      opacity: 1,
+    })
+    .from(
+      childSplit.lines,
+      {
+        duration: 1,
+        yPercent: 100,
+        ease: 'power4',
+        stagger: 0.15,
+      },
+      '>'
+    );
+  }
+
+  function textTrigger(element) {
+  //local timeline
+  var tlText = gsap.timeline();
+
+  var text = element.querySelector('.text-animation');
+  var splitLine = element.querySelector('.split-child');
+  //SplitText to work with animation line by line
+  // const childSplit = nestedLinesSplit(text, {
+  //   type: "lines",
+  //   linesClass: "split-child"
+  // });
+  const childSplit = new SplitText(text, {
+    type: 'lines',
+    linesClass: 'split-child',
+  });
+  const parentSplit = new SplitText(text, {
+    linesClass: 'split-parent',
+  });
+  // GSAP timeline to animate the text
+  //Make the text appear first
+
+  tlText
+    .to(text, {
+      duration: 0.5,
+      opacity: 1,
+    })
+    .from(
+      childSplit.lines,
+      {
+        duration: 1,
+        yPercent: 100,
+        ease: 'power4',
+        stagger: 0.15,
+      },
+      '>'
+    );
+  }
+
+  function revealImage(element) {
+  var reveal = element.querySelector('.revealer');
+  var img = element.querySelector('.img-animation');
+  // GSAP timeline to animate the reveal box and the image
+  // 		//First make the revealBox visible
+
+  //local timeline
+  var tl = gsap.timeline();
+  tl.to(reveal, {
+    duration: 0.01,
+    opacity: 1,
+  })
+    //Animate the reveal box from the bottom to the full height
+    .from(
+      reveal,
+      {
+        duration: 0.5,
+        transform: 'scale3d(1,0,1)',
+        transformOrigin: '50% 100%',
+        ease: Power4.easeOut,
+      },
+      '>'
+    )
+    // Make the image visibile
+    .to(
+      img,
+      {
+        duration: 0.01,
+        opacity: 1,
+      },
+      '>'
+    )
+    //Animate the reveal box from full height to the top with no height
+    .to(
+      reveal,
+      {
+        duration: 0.5,
+        transform: 'scale3d(1,0,1)',
+        transformOrigin: '50% 0%',
+        ease: Power4.easeOut,
+      },
+      '>'
+    );
+}
 });
