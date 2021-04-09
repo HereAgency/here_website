abc894cde46feaa7ccd87cc0ff6ee419ea896bfc
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-09 14:05:45 +1000

    Update imageHoverEffects.js
    Background changing on Hover section removed (to be added in a separate file)

--- js_files/imageHoverEffects.js
+++ js_files/imageHoverEffects.js
@@ -5,6 +5,42 @@
 //     el: document.querySelector('.scroll-container'),
 //     smooth: true
 //   })
+
+  //*************************************
+  // SECTION: BACKGROUND CHANGE ON HOVER
+  //*************************************
+//   const labelTrigger = $(".hover-section .section-wrapper a");
+//   const hoverImg = "#service-img-";
+//   const hoverLabel = "#service-lbl-";
+//   const allLabels = $("[id*=service-lbl-]");
+
+//   let imgId = 0;
+
+//   labelTrigger.mouseenter(function() {
+//     imgId = this.id.slice(-1);
+
+//     // HIGHLIGHT ONLY THE LABEL ON HOVER:
+//     allLabels.css("opacity", "0.32");
+//     $(hoverLabel+imgId+"").css("opacity", "1", );
+
+//     // SHOW IMAGE AND ANIMATE TO MOVE RIGHT
+//     $(hoverImg + imgId + "")
+//       .fadeIn()
+//       .addClass("service-lbl-active")
+//   });
+//   labelTrigger.mouseleave(function() {
+//     imgId = this.id.slice(-1);
+
+//     // LABELS BACK TO NORMAL COLOR
+//     allLabels.css("opacity", "1");
+
+//     // HIDE IMAGE
+//     // console.log("mouseleft item: "+ imgId);
+//     $(hoverImg + imgId + "")
+//       .css("opacity", "0")
+//       .removeClass("service-lbl-active")
+//   });
+  
   //*************************************
   // CODROPS ANIMATION - preloader.js
   //*************************************
@@ -102,41 +138,6 @@
       requestAnimationFrame(() => this.render());
     }
   }
-  //*************************************
-  // SECTION: BACKGROUND CHANGE ON HOVER
-  //*************************************
-  const labelTrigger = $(".hover-section .section-wrapper a");
-  const hoverImg = "#service-img-";
-  const hoverLabel = "#service-lbl-";
-  const allLabels = $("[id*=service-lbl-]");
-
-  let imgId = 0;
-
-  labelTrigger.mouseenter(function() {
-    imgId = this.id.slice(-1);
-
-    // HIGHLIGHT ONLY THE LABEL ON HOVER:
-    allLabels.css("opacity", "0.32");
-    $(hoverLabel+imgId+"").css("opacity", "1", );
-
-    // SHOW IMAGE AND ANIMATE TO MOVE RIGHT
-    $(hoverImg + imgId + "")
-      .fadeIn()
-      .addClass("service-lbl-active")
-  });
-  labelTrigger.mouseleave(function() {
-    imgId = this.id.slice(-1);
-
-    // LABELS BACK TO NORMAL COLOR
-    allLabels.css("opacity", "1");
-
-    // HIDE IMAGE
-    // console.log("mouseleft item: "+ imgId);
-    $(hoverImg + imgId + "")
-      .css("opacity", "0")
-      .removeClass("service-lbl-active")
-  });
-  
   //*************************************
   // CODROPS ANIMATION - index.js file
   //*************************************
