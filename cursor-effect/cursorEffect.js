e6d376255304f69bdd45401148cb8da31491f1e1
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-11 21:18:56 +1000

    Update cursorEffect.js

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -1,27 +1,17 @@
 
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-
-    t.style.left = n.clientX + "px",
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
+	t.style.left = e.style.left = n.clientX + "px",
+	e.style.top = t.style.top = n.clientY + "px"
 });
 
 var t = document.getElementById("cursor-dot"),
-    e = document.getElementById("cursor-img"),
-    i = document.getElementById("cursor-arrow");
+    e = document.getElementById("cursor-img");
 
 function n(x) {
-  t.classList.add("hover"), 
-    e.classList.add("hover"), 
-    i.classList.add("hover")
+  t.classList.add("hover"),e.classList.add("hover")
 }
 function s(x) {
-  t.classList.remove("hover"),
-    e.classList.remove("hover"),
-    i.classList.remove("hover")
+  t.classList.remove("hover"),e.classList.remove("hover")
 }
 s();
 for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
@@ -38,12 +28,10 @@ cursorModifiers.forEach(cursorModifier => {
   cursorModifier.addEventListener('mouseenter', function() {
     const className = this.getAttribute('cursor-class');
     bodychange.classList.add(className);
-    // t.classList.add(className);
   });
   
   cursorModifier.addEventListener('mouseleave', function() {
     const className = this.getAttribute('cursor-class');
     bodychange.classList.remove(className);
-    // t.classList.remove(className);
   });
 });
