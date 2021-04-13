30c2587c5c71767dd222945065950f7b6d19e062
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-13 11:05:12 +1000

    Update img-text_animation.js

--- js_files/img-text_animation.js
+++ js_files/img-text_animation.js
@@ -65,20 +65,27 @@ $('document').ready(function () {
       elImg.classList.add('hasClassTriggered');
     }
   });
-
-  //PAGE TRANSITION ANIMATION - TRIGGERED WHEN CLICK ON ANY LINK
+//PAGE TRANSITION LOADING ANIMATION
   let links = document.querySelectorAll('a');
   if (links){
     links.forEach ((link) => {
       link.onclick = (e) => {
         console.log('link clicked');
-        let body = document.querySelector('body');
+        let linkURL;
+        if (!e.currentTarget.href){
+             console.log(e.target.href);
+            linkURL = e.target.href;
+           }else {
+             console.log(e.currentTarget.href);
+             linkURL = e.currentTarget.href;
+           }
         e.preventDefault();
-        pageAnimation(e);
+        pageAnimation(linkURL);
       }
     });
   }
-  function pageAnimation(e){
+  function pageAnimation(linkURL){
+    // console.log(e);
     let pageLoader = document.querySelector('.pg-trans-anim');
     let pgTl = gsap.timeline();
     
@@ -92,6 +99,10 @@ $('document').ready(function () {
         transform: 'scale3d(1,0,1)',
         transformOrigin: '50% 100%',
         ease: Power4.easeOut,
+        onComplete: function(){
+          console.log('navigating');
+          window.location = linkURL;
+        }
        },'>')
         //Animate the reveal box from full height to the top with no height
       .to(pageLoader, {
@@ -99,14 +110,6 @@ $('document').ready(function () {
         transform: 'scale3d(1,0,1)',
         transformOrigin: '50% 0%',
         ease: Power4.easeOut,
-        onComplete: function(){
-           console.log('navigating');
-           if (!e.srcElement.parentElement.href){
-             window.location = e.srcElement.href;
-           }else {
-             window.location = e.srcElement.parentElement.href;
-           }
-        }
       },'>')
       .to(pageLoader,{
         duration:0.01,
