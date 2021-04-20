e3cf603512d1ed3fb73b56a4b7e5da84c2a5be3c
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-20 16:36:55 +1000

    Update cursorEffect.js

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -1,39 +1,26 @@
 
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
 	if(document.body.classList.contains('layout-switch')){
-		t.style.left = e.style.left = n.clientX + "px",
-		e.style.top = t.style.top = n.clientY + "px"
+		t.style.left = n.clientX + "px",
+		t.style.top = n.clientY + "px"
 	}
 });
 
-var t = document.getElementById("cursor-dot"),
-    e = document.getElementById("cursor-img");
+var t = document.getElementById("cursor-dot");
 
-function n(x) {
-  t.classList.add("hover"),e.classList.add("hover")
-}
-function s(x) {
-  t.classList.remove("hover"),e.classList.remove("hover")
-}
-s();
-for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-  o(r[a])
-}
-function o(t) {
-  t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-}
-
-const cursorModifiers = document.querySelectorAll('[cursor-class]');
+const cursorModifiers = document.querySelectorAll('.hover-target');
 const bodychange = document.body;
 
 cursorModifiers.forEach(cursorModifier => {
-  cursorModifier.addEventListener('mouseenter', function() {
+  $(cursorModifier).mouseenter( function() {
     const className = this.getAttribute('cursor-class');
     bodychange.classList.add(className);
+    t.classList.add("hover"); 
   });
   
-  cursorModifier.addEventListener('mouseleave', function() {
+  $(cursorModifier).mouseleave(function() {
     const className = this.getAttribute('cursor-class');
     bodychange.classList.remove(className);
+    t.classList.remove("hover");
   });
 });
