250016145b394074bacd74390d32bd6923d7109c
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-21 10:52:11 +1000

    Update img-text_animation.js
    Removed page transition. To be added in a new doc

--- js_files/img-text_animation.js
+++ js_files/img-text_animation.js
@@ -1,57 +1,57 @@
 $('document').ready(function () {  
   
-//PAGE TRANSITION LOADING ANIMATION
-  let links = document.querySelectorAll('a');
-  if (links){
-    links.forEach ((link) => {
-      link.onclick = (e) => {
-        console.log('link clicked');
-        let linkURL;
-        if (!e.currentTarget.href){
-             console.log(e.target.href);
-            linkURL = e.target.href;
-           }else {
-             console.log(e.currentTarget.href);
-             linkURL = e.currentTarget.href;
-           }
-        e.preventDefault();
-        pageAnimation(linkURL);
-      }
-    });
-  }
-  function pageAnimation(linkURL){
-    // console.log(e);
-    let pageLoader = document.querySelector('.pg-trans-anim');
-    let pgTl = gsap.timeline();
+// //PAGE TRANSITION LOADING ANIMATION
+//   let links = document.querySelectorAll('a');
+//   if (links){
+//     links.forEach ((link) => {
+//       link.onclick = (e) => {
+//         console.log('link clicked');
+//         let linkURL;
+//         if (!e.currentTarget.href){
+//              console.log(e.target.href);
+//             linkURL = e.target.href;
+//            }else {
+//              console.log(e.currentTarget.href);
+//              linkURL = e.currentTarget.href;
+//            }
+//         e.preventDefault();
+//         pageAnimation(linkURL);
+//       }
+//     });
+//   }
+//   function pageAnimation(linkURL){
+//     // console.log(e);
+//     let pageLoader = document.querySelector('.pg-trans-anim');
+//     let pgTl = gsap.timeline();
     
-    pgTl
-      .to(pageLoader, {
-        duration: 0.01,
-        display: 'block'
-      })
-      .from(pageLoader,{
-        duration: 0.7,
-        transform: 'scale3d(1,0,1)',
-        transformOrigin: '50% 100%',
-        ease: Power4.easeOut,
-        onComplete: function(){
-          console.log('navigating');
-          window.location = linkURL;
-        }
-       },'>')
-        //Animate the reveal box from full height to the top with no height
+//     pgTl
 //       .to(pageLoader, {
-//         duration: 0.8,
+//         duration: 0.01,
+//         display: 'block'
+//       })
+//       .from(pageLoader,{
+//         duration: 0.7,
 //         transform: 'scale3d(1,0,1)',
-//         transformOrigin: '50% 0%',
+//         transformOrigin: '50% 100%',
 //         ease: Power4.easeOut,
-//       },'>')
-      .to(pageLoader,{
-        duration:1,
-        display:'none'
-      }, '>');
-  }
-  // ******END PAGE ANIMATION ***********
+//         onComplete: function(){
+//           console.log('navigating');
+//           window.location = linkURL;
+//         }
+//        },'>')
+//         //Animate the reveal box from full height to the top with no height
+// //       .to(pageLoader, {
+// //         duration: 0.8,
+// //         transform: 'scale3d(1,0,1)',
+// //         transformOrigin: '50% 0%',
+// //         ease: Power4.easeOut,
+// //       },'>')
+//       .to(pageLoader,{
+//         duration:1,
+//         display:'none'
+//       }, '>');
+//   }
+//   // ******END PAGE ANIMATION ***********
   
   //Triggers images and texts (only once) when in viewport:
   inView.threshold(0.2);//triggers when element is 20% in viewport
