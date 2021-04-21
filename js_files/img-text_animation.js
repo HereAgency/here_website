552bb2dbc4d3dc61ddb020c182782958af3808b7
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-04-21 11:05:34 +1000

    Update img-text_animation.js
    Added fadeIn animation

--- js_files/img-text_animation.js
+++ js_files/img-text_animation.js
@@ -1,58 +1,5 @@
 $('document').ready(function () {  
   
-// //PAGE TRANSITION LOADING ANIMATION
-//   let links = document.querySelectorAll('a');
-//   if (links){
-//     links.forEach ((link) => {
-//       link.onclick = (e) => {
-//         console.log('link clicked');
-//         let linkURL;
-//         if (!e.currentTarget.href){
-//              console.log(e.target.href);
-//             linkURL = e.target.href;
-//            }else {
-//              console.log(e.currentTarget.href);
-//              linkURL = e.currentTarget.href;
-//            }
-//         e.preventDefault();
-//         pageAnimation(linkURL);
-//       }
-//     });
-//   }
-//   function pageAnimation(linkURL){
-//     // console.log(e);
-//     let pageLoader = document.querySelector('.pg-trans-anim');
-//     let pgTl = gsap.timeline();
-    
-//     pgTl
-//       .to(pageLoader, {
-//         duration: 0.01,
-//         display: 'block'
-//       })
-//       .from(pageLoader,{
-//         duration: 0.7,
-//         transform: 'scale3d(1,0,1)',
-//         transformOrigin: '50% 100%',
-//         ease: Power4.easeOut,
-//         onComplete: function(){
-//           console.log('navigating');
-//           window.location = linkURL;
-//         }
-//        },'>')
-//         //Animate the reveal box from full height to the top with no height
-// //       .to(pageLoader, {
-// //         duration: 0.8,
-// //         transform: 'scale3d(1,0,1)',
-// //         transformOrigin: '50% 0%',
-// //         ease: Power4.easeOut,
-// //       },'>')
-//       .to(pageLoader,{
-//         duration:1,
-//         display:'none'
-//       }, '>');
-//   }
-//   // ******END PAGE ANIMATION ***********
-  
   //Triggers images and texts (only once) when in viewport:
   inView.threshold(0.2);//triggers when element is 20% in viewport
   inView('.text-wrapper').on('enter', function (elText) {
@@ -63,12 +10,22 @@ $('document').ready(function () {
       elText.classList.add('hasClassTriggered');
     }
   });
+  //images with revealer:
   inView('.image-wrapper').on('enter', function (elImg) {
     if (elImg.classList.contains('hasClassTriggered')) {
       return;
     } else {
       revealImage(elImg);
       elImg.classList.add('hasClassTriggered');
+    }
+  });
+  //Images/sections fade in
+  inView('.fadeIn-wrapper').on('enter', function(elFadein) {
+    if (elFadein.classList.contains('hasClassTriggered')) {
+      return;
+    } else {
+      fadeInElement(elFadein);
+      elFadein.classList.add('hasClassTriggered');
     }
   });
   
@@ -156,5 +113,18 @@ $('document').ready(function () {
       },
       '>'
     );
-}
+  }
+  function fadeInElement(element) {
+    var elFadeIn = element.querySelector('.fadeIn-wrapper');
+
+    //local timeline
+    var tl = gsap.timeline();
+    
+    tl.to(
+      elFadeIn, 
+      {
+        duration: 1,
+        opacity: 1,
+      });
+  }
 });
