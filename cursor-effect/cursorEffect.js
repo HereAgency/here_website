a3fe9c80532cd99289433de8cb70325a58e0c04e
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-05-24 17:29:17 +1000

    Update cursorEffect.js
    remove cursor from hover-section

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -1,6 +1,6 @@
 
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-	if(document.body.classList.contains('layout-switch')){
+	if( (document.body.classList.contains('layout-switch')) && (!document.body.classList.contains('hover-section'))){
 		t.style.left = n.clientX + "px",
 		t.style.top = n.clientY + "px"
 	}
