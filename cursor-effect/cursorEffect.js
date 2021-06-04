f4af2eaa760ee88ffb78356967003019821be8eb
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-04 16:38:11 +1000

    Update cursorEffect.js

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -1,30 +1,39 @@
 
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-	if( (document.body.classList.contains('layout-switch')) 
-		 && (!document.body.classList.contains('hover-section'))){
+	if( (document.body.classList.contains('layout-switch')) && (!document.body.classList.contains('hover-section'))){
+		//image moving on hover:
 		gsap.to(e, 0.3, {
 		    left: n.clientX + "px",
 		    top: n.clientY + "px",
 		});
+		//red dot on cursor:
 		gsap.to(t, {
-			opacity:1
+			opacity:1,
+			left: n.clientX + "px",
+			top: n.clientY + "px"
 		});
-		t.style.left = n.clientX + "px",
-		t.style.top = n.clientY + "px"
+// 		t.style.left = n.clientX + "px",
+// 		t.style.top = n.clientY + "px"
 		//	e.style.left = n.clientX + "px",
 		// 	e.style.top = n.clientY + "px"
 	
 	}
+	//desable cursor on hover-section
+	if (document.body.classList.contains('hover-section'))){
+		gsap.to(t, {
+			opacity:0
+		});
+	}
 });
 
-$('.hover-section').mouseenter(function() {
-// 		gsap.to('.hover-section', {
-// 			cursor:default
-// 		});
-	gsap.to(t, {
-		opacity:0
-	});
-});
+// $('.hover-section').mouseenter(function() {
+// // 		gsap.to('.hover-section', {
+// // 			cursor:default
+// // 		});
+// 	gsap.to(t, {
+// 		opacity:0
+// 	});
+// });
 
 var t = document.getElementById("cursor-dot");
 var e = document.getElementById("cursor-img");
