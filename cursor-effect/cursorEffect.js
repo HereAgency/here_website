7a7da43247bd7ec4fd0e5da4c30bd611afd1cae1
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-04 16:45:29 +1000

    Update cursorEffect.js

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -5,24 +5,27 @@ document.getElementsByTagName("body")[0].addEventListener("mousemove", function(
 		    left: n.clientX + "px",
 		    top: n.clientY + "px"
 		});
-		//red dot on cursor:
-		gsap.to(t, {
-			opacity:1,
-			left: n.clientX + "px",
-			top: n.clientY + "px"
-		});
-// 		t.style.left = n.clientX + "px",
-// 		t.style.top = n.clientY + "px"
 		//	e.style.left = n.clientX + "px",
 		// 	e.style.top = n.clientY + "px"
+		
+		//red dot on cursor:
+		// gsap.to(t, {
+		// 	opacity:1,
+		// 	left: n.clientX + "px",
+		// 	top: n.clientY + "px"
+		// });
+		t.style.opacity = 1,
+		t.style.left = n.clientX + "px",
+		t.style.top = n.clientY + "px"
 	
 	}
 	//desable cursor on hover-section
 	if (document.body.classList.contains('hover-section')){
-		gsap.to(t, {
-			opacity:0,
-			duration:0.1
-		});
+		// gsap.to(t, {
+		// 	opacity:0,
+		// 	duration:0.1
+		// });
+		t.style.opacity = 0
 	}
 });
 
