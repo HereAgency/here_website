055c9deed73752348e230fb143f1792a9df1b8f4
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-14 22:05:47 +1000

    Update swiper-slider.js

--- js_files/swiper-slider.js
+++ js_files/swiper-slider.js
@@ -1,5 +1,5 @@
-// var fullWidthSwiper = new Swiper('.agency-swiper-container', {
-//   loop: true,
-//   slidesPerView: 2,
-//   spaceBetween:100
-// });
+var fullWidthSwiper = new Swiper('.agency-swiper-container', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween:100
+});
