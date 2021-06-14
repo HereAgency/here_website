3b4be305653d86afe8690b2fffe7053e33ed875a
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-14 13:31:25 +1000

    Update cursorEffect.js

--- cursor-effect/cursorEffect.js
+++ cursor-effect/cursorEffect.js
@@ -1,77 +1,93 @@
-document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-	if( (document.body.classList.contains('layout-switch')) 
-	   && (!document.body.classList.contains('desable'))
-	  ){
-		//image moving on hover:
-		gsap.to(e, 0.3, {
-		    left: n.clientX + "px",
-		    top: n.clientY + "px"
-		});
-		//Red dot
-		t.style.left = n.clientX + "px",
-		t.style.top = n.clientY + "px"
+// document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+// 	if( (document.body.classList.contains('layout-switch')) 
+// 	   && (!document.body.classList.contains('desable'))
+// 	  ){
+// 		//image moving on hover:
+// 		gsap.to(e, 0.3, {
+// 		    left: n.clientX + "px",
+// 		    top: n.clientY + "px"
+// 		});
+// 		//Red dot
+// 		t.style.left = n.clientX + "px",
+// 		t.style.top = n.clientY + "px"
 		
-	}
-});
-
-var t = document.getElementById("cursor-dot");
+// 	}
+// });
+var dot = document.getElementById("cursor-dot");
 var e = document.getElementById("cursor-img");
-var a = document.getElementById("cursor-arrow-content");
+
+(function () {
+    gsap.set(dot, {
+        xPercent: -50,
+        yPercent: -50,
+    });
+    document.addEventListener('pointermove', movecursor);
+    function movecursor(n) {
+      dot.style.left = n.clientX + "px",
+      dot.style.top = n.clientY + "px",
+        
+      gsap.to(e, 0.3, {
+        left: n.clientX + "px",
+        top: n.clientY + "px"
+      });
+    }
+})();
 
 const cursorModifiers = document.querySelectorAll('.hover-target');
 const bodychange = document.body;
 
 cursorModifiers.forEach(cursorModifier => {
   $(cursorModifier).mouseenter( function() {
-	const className = this.getAttribute('cursor-class');
-	bodychange.classList.add(className);
-	const imgName = this.getAttribute('img-name');
-	if(className == 'desable'){
-		t.style.opacity = 0
-	} else if (className == 'arrow'){
-		t.classList.add("hover");
-		gsap.set(a,  {
-		    opacity: 1,
-		    delay: 0.2,
-		    duration: 0.8
-		}); 
-	} else if(className == 'img-load'){
-		e.classList.add("hover");
-		e.classList.add(imgName);
-		t.classList.add("hover");
-		gsap.set(a,  {
-		    opacity: 1,
-		    delay: 0.2,
-		    duration: 0.8
-		}); 
-	}
+    const className = this.getAttribute('cursor-class');
+    const cursorItem = this.getAttribute('cursor-item');
+    const item = document.getElementById(cursorItem);
+    const arrow = document.getElementById('cursor-arrow');
+    bodychange.classList.add(className);
+    
+    if(className == 'desable'){
+      dot.style.opacity = 0
+    } else if (className == 'cursor-elipse'){
+      dot.classList.add("hover");
+      arrow.classList.add("active");
+      item.classList.add("active");
+      
+    } else{
+      e.classList.add("hover"); 
+      gsap.set(a,  {
+        opacity: 1,
+        delay: 0.2,
+        duration: 0.8
+      });
+    }
   });
   
   $(cursorModifier).mouseleave(function() {
-	const className = this.getAttribute('cursor-class');
-	bodychange.classList.add(className);
-	const imgName = this.getAttribute('img-name');
-	if(className == 'desable'){
-		t.style.opacity = 1
-	} else if (className == 'arrow'){
-		t.classList.remove("hover");
-		gsap.set(a,  {
-		    opacity: 0,
-		    duration: 0.2
-		}); 
-	} else if(className == 'img-load'){
-		e.classList.remove("hover");
-		e.classList.remove(imgName);
-		t.classList.remove("hover");
-		gsap.set(a,  {
-		    opacity: 0,
-		    duration: 0.2
-		}); 
-	}
+    const className = this.getAttribute('cursor-class');
+    const cursorItem = this.getAttribute('cursor-item');
+    const item = document.getElementById(cursorItem);
+    const arrow = document.getElementById('cursor-arrow');
+    bodychange.classList.add(className);
+    
+    if(className == 'desable'){
+      dot.style.opacity = 1
+    } else if (className == 'cursor-elipse'){
+      dot.classList.remove("hover");
+      arrow.classList.remove("active");
+      item.classList.remove("active");
+    } else {
+      e.classList.remove("hover"); 
+      gsap.set(a,  {
+        opacity: 0,
+        // delay: 0.2,
+        duration: 0.8
+      });
+    }
   });
 });
 
-// MAGNETIC CURSOR FOR INSTAGRAM ICON
+/************************************ 
+  MAGNETIC CURSOR FOR INSTAGRAM ICON
+*************************************/
 class MagneticCursor {
   constructor(el) {
     this.el = el;
