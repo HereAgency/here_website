79e19140339de7a4928c79611e3cb7ea594deb06
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-20 12:43:06 +1000

    Update nav-handler.js

--- js_files/nav-handler.js
+++ js_files/nav-handler.js
@@ -1,4 +1,31 @@
-//NAV COLOR CHANGE
+// NAV SCROLL OBSERVER 
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 10) {
+       $('#navbar').addClass('nav-scrolled');
+    } else {
+       $('#navbar').removeClass('nav-scrolled');
+    }
+});
+
+// NAV TRANSITIONS ON HOVER
+
+$(document).ready(function(){
+  $("#navbar").mouseenter(function(){
+    if ($( "#navbar" ).hasClass( "nav-scrolled" )){
+      $('.nav-item').css({"transform":"translateY(0px)", "opacity":"1"});
+      $('.hamburger-menu').css({"transform":"translateX(48px)", "opacity": "0"});
+    }
+  });
+  $("#navbar").mouseleave(function(){
+    if ($( "#navbar" ).hasClass( "nav-scrolled" )){
+      $('.nav-item').css({"transform":"translateY(-48px)", "opacity":"0"});
+      $('.hamburger-menu').css({"transform":"translateX(0)", "opacity":"1"});
+    }
+  });
+});
+
+//NAV COLOR CHANGE ON BACKGROUND CHANGE
 
 var navOffset = $("#navbar").offset();
 var sectionDivs = $(".ha-section");
