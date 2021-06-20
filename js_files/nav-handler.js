1b3a35b690ed5fcb98c83d3104a920100352a7da
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-06-20 14:10:06 +1000

    Update nav-handler.js

--- js_files/nav-handler.js
+++ js_files/nav-handler.js
@@ -34,7 +34,7 @@ $(document).scroll(function() {
     sectionDivs.each(function(k) {
         var sectionsOffset = $(this).offset();
         var _actPosition = sectionsOffset.top - $(window).scrollTop();
-        if (_actPosition < (navOffset.top + $('#navbar').height()/2) && _actPosition + $(this).height() - $('#navbar').height()/2 > 0) {
+        if (_actPosition < (navOffset.top + $('#navbar').height()/2) && _actPosition + $(this).outerHeight() - $('#navbar').height()/2 > 0) {
             $("#navbar").removeClass("light dark").addClass($(this).hasClass("light") ? "light" : "dark");
             return false;
         }
