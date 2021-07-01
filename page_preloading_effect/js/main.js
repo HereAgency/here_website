a940dda5ddf6498ccf9c532049c00748e68dcb5b
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-01 11:32:21 +1000

    Update main.js

--- page_preloading_effect/js/main.js
+++ page_preloading_effect/js/main.js
@@ -8,36 +8,36 @@
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
-(function() {
+// (function() {
+$('document').ready(function () {
 	var support =
 		{ animations : Modernizr.cssanimations },
 		container = document.querySelector( '.ip-container' ),
 		header = container.querySelector( '.ip-header' ),
 		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
-	    	//Modernizr.prefixed takes a string css value in the DOM style camelCase form and returns the version of that property that the browser actually supports
+		//Modernizr.prefixed takes a string css value in the DOM style camelCase form and returns the version of that property that the browser actually supports
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
-	
-	function init() {
-		var onEndInitialAnimation = function() {
-			if( support.animations ) {
-				this.removeEventListener( animEndEventName, onEndInitialAnimation );
-			}
-			startLoading();
-		};
-		// disable scrolling
-		window.addEventListener( 'scroll', noscroll );
 
-		// initial animation
-		container.classList.add('loading');
-		
-		//Check if animation has ended
+	var onEndInitialAnimation = function() {
 		if( support.animations ) {
-			container.addEventListener( animEndEventName, onEndInitialAnimation );
+			this.removeEventListener( animEndEventName, onEndInitialAnimation );
 		}
-		else {
-			onEndInitialAnimation();
-		}
+		startLoading();
+	};
+	// disable scrolling
+	window.addEventListener( 'scroll', noscroll );
+
+	// initial animation
+	container.classList.add('loading');
+
+	// check if animation has ended
+	if( support.animations ) {
+		container.addEventListener( animEndEventName, onEndInitialAnimation );
 	}
+	else {
+		onEndInitialAnimation();
+	}
+
 	function startLoading() {
 		let time=2600; // Time the loader plays
 		setTimeout(function (time) {
@@ -50,11 +50,11 @@
 					this.removeEventListener(animEndEventName, onEndHeaderAnimation);
 				}
 				document.body.classList.add('layout-switch');
-				
+
 				//Enable scroll after preloader finishes
 				window.removeEventListener('scroll', noscroll);
 			};
-			
+
 			//Check if curtain up animation has finished:
 			if (support.animations) {
 				header.addEventListener(animEndEventName, onEndHeaderAnimation);
@@ -67,6 +67,4 @@
 	function noscroll() {
 		window.scrollTo( 0, 0 );
 	}
-	init();
-
-})();
+});
