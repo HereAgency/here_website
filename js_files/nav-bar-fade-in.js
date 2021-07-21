cb12eb79c506659db660011f627bdfc154fb6c94
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-21 16:11:26 +1000

    Update nav-bar-fade-in.js

--- js_files/nav-bar-fade-in.js
+++ js_files/nav-bar-fade-in.js
@@ -5,6 +5,7 @@ function navBarAnimation() {
     //Header elements selectors
     let navBar = document.querySelectorAll('.navbar');
     let navBarMobile = document.querySelectorAll('.mobile-menu-section');
+    let navMobileHeight = document.querySelectorAll('.container.is--nav');
 	
     // GSAP timeline to animate the text
     tlNav
@@ -19,10 +20,15 @@ function navBarAnimation() {
     }, '>')
       .fromTo(navBarMobile, {
 	opacity: 0,
-	yPercent: -100,
     }, {
         duration: 0.5,
 	opacity: 1,
+	ease: Power1.easeOut
+    }, '>')
+      .fromTo(navMobileHeight, {
+	yPercent: -100,
+    }, {
+        duration: 0.5,
 	yPercent: 0,
 	ease: Power1.easeOut
     }, '>');
