db03a3e83ccb69dfff3cafcf6203391c39dc49c2
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-21 16:25:08 +1000

    Update nav-bar-fade-in.js

--- js_files/nav-bar-fade-in.js
+++ js_files/nav-bar-fade-in.js
@@ -7,29 +7,52 @@ function navBarAnimation() {
     let navBarMobile = document.querySelectorAll('.mobile-menu-section');
     let navMobileHeight = document.querySelectorAll('.container.is--nav');
 	
+    //Responsive
+    var isDesktop = window.matchMedia("(min-width: 501px)");
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+	
+	
     // GSAP timeline to animate the text
-    tlNav
-      .fromTo(navBar, {
-	opacity: 0,
-	yPercent: -100,
-    }, {
-	duration: 0.5,
-	opacity: 1,
-	yPercent: 0,
-	ease: Power1.easeOut
-    })
-      .fromTo(navBarMobile, {
-	opacity: 0,
-    }, {
-        duration: 0.5,
-	opacity: 1,
-	ease: Power1.easeOut
-    })
-      .fromTo(navMobileHeight, {
-	yPercent: -100,
-    }, {
-        duration: 0.5,
-	yPercent: 0,
-	ease: Power1.easeOut
-    }, '<');
+    //Desktop
+    if (x.matches) {
+	tlNav
+    	.fromTo(navBar, {
+		opacity: 0,
+		yPercent: -100,
+      	}, {
+		duration: 0.5,
+		opacity: 1,
+		yPercent: 0,
+		ease: Power1.easeOut
+      	});
+    }
+    //Mobile
+    else {
+	tlNav
+	//Fade in
+	.fromTo(navBarMobile, {
+		opacity: 0,
+	}, {
+		duration: 0.5,
+		opacity: 1,
+		ease: Power1.easeOut
+	})
+	//Slide from top
+      	.fromTo(navMobileHeight, {
+		yPercent: -100,
+	    }, {
+		duration: 0.5,
+		yPercent: 0,
+		ease: Power1.easeOut
+    	}, '<');
+    }
 }
