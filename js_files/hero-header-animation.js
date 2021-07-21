6d46a2514f827231b39606eec1346902c3898107
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-21 22:20:07 +1000

    Update hero-header-animation.js

--- js_files/hero-header-animation.js
+++ js_files/hero-header-animation.js
@@ -5,53 +5,82 @@ function heroHeadAnimation() {
     //Header elements selectors
     let mainWrapper = document.querySelectorAll('.main-text-wrapper-header');
     let blurbWrapper = document.querySelectorAll('.desc-text-wrapper-header');
+	
+    //Home page selectors for Mobile
+    let homeMainWrapper = document.querySelectorAll('.home-main-text-animation');
+    let homeBlurbWrapper = document.querySelectorAll('.home-desc-text-animation');
     let homeWorkHeading = document.querySelectorAll('.section-heading.cs-animation');
 	
-    // GSAP timeline to animate the text
-    tlHero
-      .fromTo(mainWrapper, {
-		opacity: 0,
-		y: 64,
-		skewY: "3deg",
-	}, {
-        	duration: 0.4,
-		opacity: 1,
-		y: 0,
-		skewY: "0deg",
-   		ease: Power1.easeOut,	
-      }, '>');
-    if (blurbWrapper){
-	tlHero
-		.fromTo(blurbWrapper, {
-		opacity: 0,
-		y: 64,
-		skewY: "3deg",
-	}, {
-		duration: 0.4,
-		delay: 0.1,
-		opacity: 1,
-		y: 0,
-		skewY: "0deg",
-		ease: Power1.easeOut,	
-	}, '>');
-    }
-
     //Responsive
     let isMobile = window.matchMedia("(max-width: 500px)");
-    //Home page Work heading animaiton on Mobile
-    if (homeWorkHeading && isMobile.matches){
-	tlHero
-		.fromTo(homeWorkHeading, {
-		opacity: 0,
-		y: 64,
-		skewY: "3deg",
-	}, {
-		duration: 0.4,
-		delay: 0.1,
-		opacity: 1,
-		y: 0,
-		skewY: "0deg",
-		ease: Power1.easeOut,	
-	}, '>');
-    }
+
+    // GSAP timeline to animate the text
+		tlHero
+			.fromTo(mainWrapper, {
+			opacity: 0,
+			y: 64,
+			skewY: "3deg",
+		}, {
+			duration: 0.4,
+			opacity: 1,
+			y: 0,
+			skewY: "0deg",
+			ease: Power1.easeOut,	
+		}, '>');
+		// If there's blurb heading:
+		if (blurbWrapper){
+			tlHero
+				.fromTo(blurbWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "3deg",
+			}, {
+				duration: 0.4,
+				delay: 0.1,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>');
+		}
+
+		//Home page on Mobile
+		if (homeWorkHeading && isMobile.matches){
+			tlHero
+				.fromTo(homeMainWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "3deg",
+			}, {
+				duration: 0.4,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>')
+				.fromTo(homeBlurbWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "3deg",
+			}, {
+				duration: 0.4,
+				delay: 0.1,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>')
+				.fromTo(homeWorkHeading, {
+				opacity: 0,
+				y: 64,
+				skewY: "3deg",
+			}, {
+				duration: 0.4,
+				delay: 0.1,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>');
+		}
 }
