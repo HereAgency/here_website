891ae6f79336b46042bffa809a6c07e1f566bd8b
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-21 14:43:55 +1000

    Update prealoader.js

--- js_files/prealoader.js
+++ js_files/prealoader.js
@@ -6,7 +6,8 @@ let support =
 	animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
 	//Modernizr.prefixed takes a string css value in the DOM style camelCase form and returns the version of that property that the browser actually supports
 	animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
-			
+
+//When document is ready	
 $('document').ready(function () {
 	let onEndInitialAnimation = function() {
 		if( support.animations ) {
@@ -34,4 +35,38 @@ $('document').ready(function () {
 //function to desable scroll
 function noscroll() {
 	window.scrollTo( 0, 0 );
+}
+
+//Function for the preloader
+function startLoading() {
+  let time; // Time the animation plays
+  if(container.classList.contains('loading')){
+    time=5000; //if showing preloader
+  } else {
+    time = 500; //if showing only 'curtain up' animation
+  }
+  setTimeout(function (time) {
+    if(container.classList.contains('loading')){
+      container.classList.remove('loading');
+    }
+    container.classList.add('loaded');
+
+    var onEndHeaderAnimation = function (ev) {
+      if (support.animations) {
+        if (ev.target !== header) return;
+        this.removeEventListener(animEndEventName, onEndHeaderAnimation);
+      }
+      document.body.classList.add('layout-switch');
+
+      //Enable scroll after preloader finishes
+      window.removeEventListener('scroll', noscroll);
+    };
+
+    //Check if curtain up animation has finished:
+    if (support.animations) {
+      header.addEventListener(animEndEventName, onEndHeaderAnimation);
+    } else {
+      onEndHeaderAnimation();
+    }
+  }, time);
 }
