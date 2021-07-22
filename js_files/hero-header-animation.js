debac7045ce2a2eaeab8b30a4e177564e3b3282e
Here Agency <61567610+HereAgency@users.noreply.github.com>
2021-07-22 14:22:05 +1000

    Update hero-header-animation.js

--- js_files/hero-header-animation.js
+++ js_files/hero-header-animation.js
@@ -1,20 +1,94 @@
 function heroHeadAnimation() {
-    //local timeline
-    let tlHero = gsap.timeline();
+	//local timeline
+	let tlHero = gsap.timeline();
 
-    //Header elements selectors
-    let mainWrapper = document.querySelectorAll('.main-text-wrapper-header');
-    let blurbWrapper = document.querySelectorAll('.desc-text-wrapper-header');
-	
-    //Home page selectors for Mobile
-    let homeMainWrapper = document.querySelectorAll('.home-main-text-animation');
-    let homeBlurbWrapper = document.querySelectorAll('.home-desc-text-animation');
-    let homeWorkHeading = document.querySelectorAll('.section-heading.cs-animation');
-	
-    //Responsive
-    let isMobile = window.matchMedia("(max-width: 500px)");
+	//Header elements selectors
+	let mainWrapper = document.querySelectorAll('.main-text-wrapper-header');
+	let blurbWrapper = document.querySelectorAll('.desc-text-wrapper-header');
 
-    // GSAP timeline to animate the text
+	//Home page selectors for Mobile
+	let homeMainWrapper = document.querySelectorAll('.home-main-text-animation');
+	let homeBlurbWrapper = document.querySelectorAll('.home-desc-text-animation');
+	let homeWorkHeading = document.querySelectorAll('.section-heading.cs-animation');
+
+	//Check screen size
+	let isMobile = window.matchMedia("(max-width: 500px)");
+
+	// MOBILE
+	if(isMobile.matches){
+		//If oage is Home page:
+		if (homeWorkHeading){
+			tlHero
+				.fromTo(homeMainWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "10deg",
+			}, {
+				duration: 0.4,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>')
+				.fromTo(homeBlurbWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "10deg",
+			}, {
+				duration: 0.4,
+				delay: 0.1,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>')
+				.fromTo(homeWorkHeading, {
+				opacity: 0,
+				y: 64,
+				skewY: "10deg",
+			}, {
+				duration: 0.4,
+				delay: 0.1,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>');
+		} 
+		//All other pages mobile
+		else {
+			tlHero
+				.fromTo(mainWrapper, {
+				opacity: 0,
+				y: 64,
+				skewY: "10deg",
+			}, {
+				duration: 0.4,
+				opacity: 1,
+				y: 0,
+				skewY: "0deg",
+				ease: Power1.easeOut,	
+			}, '>');
+			// If there's blurb heading:
+			if (blurbWrapper){
+				tlHero
+					.fromTo(blurbWrapper, {
+					opacity: 0,
+					y: 64,
+					skewY: "10deg",
+				}, {
+					duration: 0.4,
+					delay: 0.1,
+					opacity: 1,
+					y: 0,
+					skewY: "0deg",
+					ease: Power1.easeOut,	
+				}, '>');
+			}
+		}
+	}
+	// DESKTOP:
+	else {
 		tlHero
 			.fromTo(mainWrapper, {
 			opacity: 0,
@@ -43,44 +117,5 @@ function heroHeadAnimation() {
 				ease: Power1.easeOut,	
 			}, '>');
 		}
-
-		//Home page on Mobile
-		if (homeWorkHeading && isMobile.matches){
-			tlHero
-				.fromTo(homeMainWrapper, {
-				opacity: 0,
-				y: 64,
-				skewY: "3deg",
-			}, {
-				duration: 0.4,
-				opacity: 1,
-				y: 0,
-				skewY: "0deg",
-				ease: Power1.easeOut,	
-			}, '>')
-				.fromTo(homeBlurbWrapper, {
-				opacity: 0,
-				y: 64,
-				skewY: "3deg",
-			}, {
-				duration: 0.4,
-				delay: 0.1,
-				opacity: 1,
-				y: 0,
-				skewY: "0deg",
-				ease: Power1.easeOut,	
-			}, '>')
-				.fromTo(homeWorkHeading, {
-				opacity: 0,
-				y: 64,
-				skewY: "3deg",
-			}, {
-				duration: 0.4,
-				delay: 0.1,
-				opacity: 1,
-				y: 0,
-				skewY: "0deg",
-				ease: Power1.easeOut,	
-			}, '>');
-		}
+	}
 }
